2329a532e34b319ddae1e887774b4195a9e6dffa
Yago Fontoura do Rosario <yago.rosario@hotmail.com.br>
2019-08-23 21:42:52 +0200

    User Authentication

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -37,3 +37,14 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin)
+        next();
+
+    else {
+        var err = new Error('Admin status required!');
+        err.status = 403;
+        return next(err);
+    }
+};
