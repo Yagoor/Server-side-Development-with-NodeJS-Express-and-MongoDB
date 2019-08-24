97a1e86ad002c8d79b2828dd749890f6ff68503a
Yago Fontoura do Rosario <yago.rosario@hotmail.com.br>
2019-08-24 18:26:29 +0200

    Backend as a Service

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -18,6 +18,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter')
 
 const Dishes = require('./models/dishes');
 
@@ -84,6 +85,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
